aa9a422fcfa657a4f3b700fbf83b0da4b489f25c
JolaWalach <joanna.walach@op.pl>
2022-01-03 18:18:18 +0100

    in progress

---
+++ script.js
@@ -0,0 +1,41 @@
+async function fetchApi() {
+    const countriesAPI = 'https://restcountries.com/v2/all/';
+    await fetch(countriesAPI)
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            console.log(data)
+            printData(data)
+        })
+        .catch((err) => {
+            console.log(err)
+        });
+}
+
+function printData(data) {
+    for (let i = 0; i < 10; i++) {
+        console.log(data);
+        var wrapper = document.querySelector(".wrapper");
+        var card = document.createElement("div");
+        card.className = "card";
+        wrapper.appendChild(card);
+            var name = document.createElement("p");
+            card.appendChild(name);
+            name.innerText = data[i].name;
+            var capital = document.createElement("p");
+            card.appendChild(capital);
+            capital.innerText = data[i].capital;
+            var area = document.createElement("p");
+            card.appendChild(area);
+            area.innerText = data[i].area;
+            var languages = document.createElement("p");
+            card.appendChild(languages);
+            languages.innerText = data[i].languages[0].name;
+            var population = document.createElement("p");
+            card.appendChild(population);
+            population.innerText = data[i].population;
+    }
+}
+fetchApi();
+
